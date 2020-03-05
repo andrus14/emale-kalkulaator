b5445cfa41057f35a7c6719529518c51bdc62416
Andrus Naulainen <andrus.naulainen@gmail.com>
2020-03-05 15:18:29 +0200

    JavaScript add rows and total

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ let occupations = {
     53: {name: 'Lapsehoidja', rate: 0}
 }
 
+let total = 0
+
 const occupationSelect = document.querySelector('#occupation-select')
-const rateDiv = document.querySelector('#container')
+const container = document.querySelector('#container')
+const hourInput = document.querySelector('#hour-input')
+const submitBtn = document.querySelector('#submit')
+const totalDiv = document.querySelector('#total')
 
 for ( const prop in occupations ) {
     let option = document.createElement('option')
@@ -18,17 +23,28 @@ for ( const prop in occupations ) {
 fetch('http://andmebaas.stat.ee/sdmx-json/data/PA627/19+23+35+39.3.1/all?startTime=2014&endTime=2014&dimensionAtObservation=allDimensions')
 .then(response => response.json())
 .then(data => {
-    console.log(data.structure.dimensions.observation[0])
     data.structure.dimensions.observation[0].values.forEach((el, i) => {
         occupationKey = el.name.split(' ')[0]
         key = i + ':0:0:0'
         occupationRate = data.dataSets[0].observations[key][0]
         occupations[occupationKey].rate = occupationRate
     })
-    console.log(occupations)
 
-    occupationSelect.addEventListener('change', function(e) {
-        selectedKey = e.currentTarget.value
-        rateDiv.innerHTML = occupations[selectedKey].rate
+    submitBtn.addEventListener('click', function(e) {
+
+        selectedKey = occupationSelect.value
+
+        console.log(selectedKey)
+        console.log(occupations[selectedKey].rate)
+        console.log(occupations[selectedKey].name)
+        console.log(hourInput.value)
+
+        let pay = Math.round(hourInput.value * occupations[selectedKey].rate * 100) / 100
+        total += pay
+        rowDiv = document.createElement('div')
+        rowDiv.innerHTML = '<div>' + occupations[selectedKey].name + '</div><div>' + pay.toString().replace('.', ',') + ' €</div>'
+        container.append(rowDiv)
+
+        totalDiv.innerHTML = 'Kokku: ' + total.toString().replace('.', ',') + '€'
     })
 })
